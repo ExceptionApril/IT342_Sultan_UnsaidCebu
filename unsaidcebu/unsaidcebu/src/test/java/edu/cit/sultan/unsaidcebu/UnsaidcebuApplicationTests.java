5941351d2c41703f4cb18ed5e97513e54a3c00b7
LAB13_USER ON WS35 <L13Y12W35@LabHCI.net>
2026-03-07 13:17:22 +0800

    IT342 Phase 1 – Partial

---
+++ unsaidcebu/unsaidcebu/src/test/java/edu/cit/sultan/unsaidcebu/UnsaidcebuApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.sultan.unsaidcebu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsaidcebuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
